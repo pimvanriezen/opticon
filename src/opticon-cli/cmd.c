5d9ebe8fccb6770b79110ed1c603eb454c64c9dd
Pim van Riezen <pi@midilab.nl>
2021-04-07 17:09:28 +0200

    scaling2

--- src/opticon-cli/cmd.c
+++ src/opticon-cli/cmd.c
@@ -752,14 +752,14 @@ int cmd_get_record (int argc, const char *argv[]) {
     printf ("  CPU Usage:\n");
     cmd_print_graph ("cpu","usage", 76, 2);
     printf ("\033[2m  ^     ^     ^      ^     ^     ^      ^     ^     "
-              "^      ^     ^     ^\033[0m\n");
+              "^      ^     ^     ^     ^\033[0m\n");
     printf ("  %-36s    %-36s\n", "Network in", "Network out");
     cmd_print_graph ("net","input", 36, 2);
     printf ("\033[6A");
     cmd_print_graph ("net","output", 36, 42);
     printf ("\033[2m"
-            "  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  "
-            "  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  "
+            "    ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^    "
+            "    ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  "
             "\033[0m\n");
     print_line();
 
