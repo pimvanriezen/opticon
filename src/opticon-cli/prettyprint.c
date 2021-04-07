62e20976991f076c82861f12ed92d12febc26c75
Pim van Riezen <pi@midilab.nl>
2021-04-07 18:59:09 +0200

    stop clipping

--- src/opticon-cli/prettyprint.c
+++ src/opticon-cli/prettyprint.c
@@ -113,8 +113,8 @@ void print_graph (int width, int height, int ind, double minmax, double *data) {
     for (y=0; y<height; ++y) {
         printf ("\033[%iC", ind);
         printf ("\033[38;2;%.0f;%.0f;%.0fm"
-                "\033[48;2;%.0f;%.0f;%.0fm", (b<255)?r:90, g, (b<255)?b:100,
-                gr, gr, gr);
+                "\033[48;2;%.0f;%.0f;%.0fm",
+                r, g, b, gr, gr, gr);
         for (x=0; x<width; ++x) {
             printf ("%s", bars[map[x+(y*width)]]);
         }
