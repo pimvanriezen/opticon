b3ce270d54fd2679837d2df4fe15bd8392e629dc
Pim van Riezen <pi@inimini.office.newvm.com>
2022-05-04 11:02:13 +0200

    Assume home/rpmbuild/SOURCES or we'll go nuts

--- pkg/mkrpm.sh
+++ pkg/mkrpm.sh
@@ -4,7 +4,7 @@ version=$(echo "$versionstr" | cut -f1 -d-)
 release=$(echo "$versionstr" | cut -f2 -d-)
 
 mkdir -p pkg/tmp
-git archive --format=tar.gz -o pkg/tmp/opticon-${versionstr}.tar.gz --prefix=opticon-${versionstr}/ master
+git archive --format=tar.gz -o ~/rpmbuild/SOURCES/opticon-${versionstr}.tar.gz --prefix=opticon-${versionstr}/ master
 sed -e "s/<<VERSION>>/$version/g;s/<<RELEASE>>/$release/g" \
     < pkg/opticon-agent.spec.in > pkg/tmp/opticon-agent.spec
 rpmbuild -ba pkg/tmp/opticon-agent.spec
