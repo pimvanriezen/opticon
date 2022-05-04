aaea1e1487f98470dde07052bd91d56ac44bcd47
Pim van Riezen <pi@inimini.office.newvm.com>
2022-05-04 10:29:57 +0200

    Add mkrpm script

---
+++ pkg/mkrpm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+version=$(git describe --tags | sed -e 's/-g.*//')
+sed -e 's/<<VERSION>>/$version/g' < pkg/opticon-agent.spec.in \
+                                  > pkg/opticon-agent.spec
+rpmbuild -bp pkg/opticon-agent.spec
+rm -f pkg/opticon-agent.spec
