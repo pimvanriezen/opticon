2746741c46216a7a0d37c852e2e08d15a3e6973e
Pim van Riezen <pi@inimini.office.newvm.com>
2022-05-04 10:37:50 +0200

    satisfy the rpmbuild gods

--- pkg/mkrpm.sh
+++ pkg/mkrpm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-version=$(git describe --tags | sed -e 's/-g.*//')
-sed -e "s/<<VERSION>>/$version/g" < pkg/opticon-agent.spec.in \
-                                  > pkg/opticon-agent.spec
+versionstr=$(git describe --tags | sed -e 's/-g.*//')
+version=$(echo "$versionstr" | cut -f1 -d-)
+release=$(echo "$versionstr" | cut -f2 -d-)
+
+sed -e "s/<<VERSION>>/$version/g;s/<<RELEASE>>/$release/g" \
+    < pkg/opticon-agent.spec.in > pkg/opticon-agent.spec
 rpmbuild -bp pkg/opticon-agent.spec
 rm -f pkg/opticon-agent.spec
