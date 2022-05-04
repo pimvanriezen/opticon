4e25540d68d4caca3b7165bd9332c6d033200f97
Pim van Riezen <pi@inimini.office.newvm.com>
2022-05-04 10:59:25 +0200

    Can't beat them, so join them, build a source tarball to satisfy rpmbuild

--- pkg/mkrpm.sh
+++ pkg/mkrpm.sh
@@ -3,7 +3,8 @@ versionstr=$(git describe --tags | sed -e 's/-g.*//')
 version=$(echo "$versionstr" | cut -f1 -d-)
 release=$(echo "$versionstr" | cut -f2 -d-)
 
+mkdir -p pkg/tmp
+git archive --format=tar.gz -o pkg/tmp/opticon-${versionstr}.tar.gz --prefix=opticon-${versionstr}/ master
 sed -e "s/<<VERSION>>/$version/g;s/<<RELEASE>>/$release/g" \
-    < pkg/opticon-agent.spec.in > pkg/opticon-agent.spec
-rpmbuild --noprep pkg/opticon-agent.spec
-rm -f pkg/opticon-agent.spec
+    < pkg/opticon-agent.spec.in > pkg/tmp/opticon-agent.spec
+rpmbuild -ba pkg/tmp/opticon-agent.spec
