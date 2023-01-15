a23612dca216f375dda6365c71c97f2f2a11e0a6
Pim van Riezen <pi@midilab.nl>
2023-01-15 16:44:26 +0100

    wtf

--- src/opticon-agent/mkversion.sh
+++ src/opticon-agent/mkversion.sh
@@ -3,6 +3,10 @@ VERSION=$(git describe --tags  2>/dev/null | cut -f1 -d' ' | cut -f1-2 -d-)
 if [ -z "$VERSION" ]; then
   # we're in rpmbuild, hopefully
   VERSION=$(pwd | sed -e 's@/opticon-agent/@/@;s/.*opticon-agent-//;s/.*opticon-//;s@/.*@@')
+  echo "-------------"
+  echo "Version: $VERSION"
+  echo "pwd: $(pwd)"
+  echo "-------------"
 fi
 cat > version.c << _EOF_
 const char *VERSION = "${VERSION}";
