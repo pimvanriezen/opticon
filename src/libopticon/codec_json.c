5b23d1e67f7abf0ddf520b1904c72855f3c16f5c
Pim van Riezen <pi@midilab.nl>
2023-01-23 17:04:19 +0100

    one more more check

--- src/libopticon/codec_json.c
+++ src/libopticon/codec_json.c
@@ -12,11 +12,11 @@
   * \param into The ioport to use */
 /*/ ======================================================================= /*/
 void jsoncodec_dump_val (metertype_t type, meter *m, int pos, ioport *into) {
-    if ((m->count < pos) || (m->count > SZ_EMPTY_VAL)) {
+    if (m->count > SZ_EMPTY_VAL) {
         ioport_write (into, "null", 4);
         return;
     }
-    if (m->count == SZ_EMPTY_VAL) {
+    if ((m->count < pos) || (m->count == SZ_EMPTY_VAL)) {
         if (pos>0) return;
         switch (type) {
             case MTYPE_INT:
