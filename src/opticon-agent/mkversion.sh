313b65570c067537802c790da0a79b5ac2eceb17
Pim van Riezen <pi@midilab.nl>
2023-01-30 16:38:57 +0100

    mkversion fix

--- src/opticon-agent/mkversion.sh
+++ src/opticon-agent/mkversion.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 GITTAG=$(git describe --tags)
 if [ ! $? = 0 ]; then
-  exit 1
-fi
-VERSION=$(echo "$GITTAG" | cut -f1 -d' ' | cut -f1-2 -d-)
-if [ -z "$VERSION" ]; then
-  # we're in rpmbuild, hopefully
   VERSION=$(pwd | sed -e 's@/opticon-agent$@/@;s/.*opticon-agent-//;s/.*opticon-//;s@/.*@@;s/-0$//')
-  echo "-------------"
-  echo "Version: $VERSION"
-  echo "pwd: $(pwd)"
-  echo "-------------"
+else
+  VERSION=$(echo "$GITTAG" | cut -f1 -d' ' | cut -f1-2 -d-)
 fi
 cat > version.c << _EOF_
 const char *VERSION = "${VERSION}";
