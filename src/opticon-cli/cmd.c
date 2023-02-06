8a759fb400e58d4d478c4fe5b8e37680b9676f14
Pim van Riezen <pi@midilab.nl>
2023-02-06 08:56:09 +0100

    tweak

--- src/opticon-cli/cmd.c
+++ src/opticon-cli/cmd.c
@@ -1158,7 +1158,7 @@ int cmd_get_record (int argc, const char *argv[]) {
             vartype df_tp[] = {
                 VAR_STR,VAR_INT,VAR_STR,VAR_DOUBLE,VAR_STR,VAR_NULL
             };
-            int df_wid[] = {24, 14, 6, 8, 0};
+            int df_wid[] = {28, 12, 6, 8, 0};
             int df_div[] = {0, (1024), 0, 0, 0, 0};
             const char *df_suf[] = {""," GB", "", " %", "", ""};
     
