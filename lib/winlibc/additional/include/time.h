9df9dc36bf7ee6923d7d485cd82f13455c707345
Maarten <maarten@bitethecode.com>
2023-12-06 17:27:05 +0100

    Windows add missing function declarations in time.h

--- lib/winlibc/additional/include/time.h
+++ lib/winlibc/additional/include/time.h
@@ -6,6 +6,9 @@
 
 time_t timegm(const struct tm *time);
 
+char *strptime(const char *s, const char *format, struct tm *tm);
+struct tm *gmtime_r(const time_t *timep, struct tm *result);
+
 // @note if _POSIX_THREAD_SAFE_FUNCTIONS is defined, MinGW defines localtime_r() as a forward to localtime_s() which is a forward to _localtime64_s()
 #ifndef _POSIX_THREAD_SAFE_FUNCTIONS
 	struct tm *localtime_r(const time_t *seconds, struct tm *result);
